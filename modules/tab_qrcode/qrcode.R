72cc80484d917c113954068f4c2ebbaad9a52fa8
David Barkemeyer <david.barkemeyer@gmx.de>
2021-01-22 14:53:11 +0100

    Add basic qrcode generation

--- modules/tab_qrcode/qrcode.R
+++ modules/tab_qrcode/qrcode.R
@@ -8,7 +8,13 @@ qrcode_ui <- function(id) {
         width = NULL,
         status = "success",
         title = "QR-Code generieren",
-        solidHeader = TRUE
+        solidHeader = TRUE,
+        shiny::uiOutput(
+          outputId = ns("type")
+        ),
+        shiny::plotOutput(
+          outputId = ns("qrcode")
+        )
       )
     )
   )
@@ -20,6 +26,27 @@ qrcode_server <- function(id, .values) {
     function(input, output, session) {
 
       ns <- session$ns
+
+      types_r <- shiny::reactive({
+        .values$update$type()
+        db_get_types(.values$db)
+      })
+
+      output$type <- shiny::renderUI({
+        shiny::selectInput(
+          inputId = ns("type"),
+          label = "Typ",
+          choices = types_r()
+        )
+      })
+
+      link_r <- shiny::reactive({
+        "code" %_% shiny::req(input$type)
+      })
+
+      output$qrcode <- shiny::renderPlot({
+        qrcode::qrcode_gen(link_r())
+      })
     }
   )
 }
