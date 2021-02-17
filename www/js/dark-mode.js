bb0b8b7df73ec32b8191580f237660eb2de91e4a
David Barkemeyer <david.barkemeyer@gmx.de>
2021-02-17 23:26:05 +0100

    Read dark mode from device

--- www/js/dark-mode.js
+++ www/js/dark-mode.js
@@ -1,5 +1,7 @@
 $(document).on('shiny:connected', function() {
-  if (!$('body').hasClass('dark-mode')) {
+  const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  if (!$('body').hasClass('dark-mode') && !isDark) {
     $('#customSwitch1').click();
   }
 });
