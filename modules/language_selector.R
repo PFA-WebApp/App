90257aeeaf38c08d66e095b05497fa64d34b5e3b
David Barkemeyer <david.barkemeyer@gmx.de>
2021-04-27 09:51:07 +0200

    Add class dropdown-flag to dropdown-toggle

--- modules/language_selector.R
+++ modules/language_selector.R
@@ -5,7 +5,7 @@ language_selector <- function(inputId, choices = c("de", "en")) {
     `data-choices` = paste0(choices, collapse = ","),
     `data-selected` = choices[1],
     htmltools::tags$a(
-      class = "dropdown-toggle",
+      class = "dropdown-toggle dropdown-flag",
       `data-toggle` = "dropdown",
       `aria-haspopup` = "true",
       `aria-expanded` = "false"
